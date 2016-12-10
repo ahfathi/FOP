16591bcc672baa6c0bec8fea122c09692ee9e25e
alirezasgr1999 <alireza.samiei78@student.sharif.edu>
2016-12-10 17:12:48 +0330

    isprime done!

--- main.c
+++ main.c
@@ -1,7 +1,15 @@
 #include<stdio.h>
 
 int isPrime(int num) {
-
+int i;
+if(num==1)
+return 0;
+if(num==2)
+return 1;
+for(i=2;i<num;i++)
+if(num%i==0)
+return 0;
+return 1;
 }
 
 int absVal(int num) {
